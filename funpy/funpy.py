25835639c9cc4c9a375d3f8c3eb0479a1d8da3d3
Brandon Maday <brandon.maday@gmail.com>
2019-02-05 02:47:03 -0500

    extra features

--- funpy/funpy.py
+++ funpy/funpy.py
@@ -3,6 +3,15 @@ from functools import reduce
 # Functors?
 identity = lambda x: x
 
+# Operations
+inc = lambda x: x + 1
+merge = lambda f: lambda g: {**f, **g}
+trim = lambda x: x.strip() if x is not None else x
+
+# First class functions
+map = lambda fn: lambda arr: [fn(a) for a in arr]
+filter = lambda fn: lambda arr: [a for a in arr if fn(a)]
+
 # Composition
 compose2 = lambda f, g: lambda x: f(g(x))
 compose = lambda *fns: reduce(compose2, fns, identity)
@@ -10,12 +19,17 @@ pipe = lambda *fns: reduce(compose2, fns[::-1], identity)
 
 # Accessors
 prop = lambda key: lambda adict: adict[key] if key in adict else None
+attr = lambda name: lambda cls: getattr(cls, name, None)
 
 # Comparisions
 ifElse = lambda ifFn, when, unless: lambda x: when(x) if ifFn(x) else unless(x)
 when = lambda ifFn, when: ifElse(ifFn, when, identity)
 unless = lambda ifFn, unless: ifElse(ifFn, identity, unless)
 propEq = lambda key, val: lambda x: prop(key)(x) == val
+isNone = lambda x: x is None
+isNotNone = lambda x: x is not None
+emptyString = lambda x: x=="" or x == None
+trimEmpty = compose(emptyString, trim)
 
 # Monads
 result = lambda ok: lambda data: {"ok": ok, "data": data}
@@ -23,11 +37,20 @@ resultGood = result(True)
 resultBad = result(False)
 resultData = prop("data")
 resultIsOk = propEq("ok", True)
-chainIfElse = lambda when, unless: ifElse(
-    resultIsOk, compose(when, resultData), compose(unless, resultData)
+chain = lambda when, unless: ifElse(resultIsOk, when, unless)
+chainIfElse = lambda when, unless: chain(
+    compose(when, resultData), compose(unless, resultData)
 )
-chainWhen = lambda when: chainIfElse(when, identity)
-chainUnless = lambda when: chainIfElse(identity, when)
-
-# Operations
-inc = lambda x: x + 1
+chainWhen = lambda when: chain(compose(when, resultData), identity)
+chainUnless = lambda unless: chain(identity, compose(unless, resultData))
+maybe = lambda ifFn, when, unless: ifElse(
+    ifFn, compose(resultGood, when), compose(resultBad, unless)
+)
+check = lambda prop, fn, msg: {"prop": prop, "fn": fn, "msg": msg}
+validate = lambda checks: lambda data: compose(
+    maybe(lambda x: len(x)==0, lambda noErr: data, identity),
+    lambda data: [
+        c["msg"] for c in checks
+        if c["fn"](prop(c["prop"])(data))
+    ]
+)(data)
--- funpy/tests/test_funpy.py
+++ funpy/tests/test_funpy.py
@@ -4,10 +4,27 @@ def test_identity():
     assert fp.identity(10) == 10
     assert fp.identity("cat") == "cat"
     assert fp.identity(32.2) != 32
+    
+def test_inc():
+    assert fp.inc(1) == 2
 
+def test_merge():
+    assert fp.merge({"a": 1, "b": 2, })({"c": 3}) == {"a": 1, "b": 2, "c": 3}
+    assert fp.merge({"a": 1, "b": 2, })({"b": 3}) == {"a": 1, "b": 3}
+
+def test_trim():
+    assert fp.trim("  ") == ""
+    assert fp.trim("") == ""
+    assert fp.trim(" a ") == "a"
 def test_prop():
     assert fp.prop("name")({"name": "bob", "age": 34}) == "bob"
     assert fp.prop("shoesize")({"name": "bob", "age": 34}) == None
+
+def test_attr():
+    class person():
+        def __init__(self):
+            self.name="bob"
+    assert(fp.attr("name")(person()) == "bob")
 
 def test_conditional():
     isOne = lambda x: x == 1
@@ -23,6 +40,31 @@ def test_compare():
     assert fp.propEq("name", "bob")({"name": "bob"}) == True
     assert fp.propEq("name", "john")({"name": "bob"}) == False
     assert fp.propEq("age", 34)({"name": "bob"}) == False
+
+def test_isNone():
+    assert(fp.isNone(None) == True)
+    assert(fp.isNone("bob") == False)
+
+def test_isNotNone():
+    assert(fp.isNotNone(None) == False)
+    assert(fp.isNotNone("bob") == True)
+
+def test_emptyString():
+    assert fp.emptyString("  ") == False
+    assert fp.emptyString("") == True
+    assert fp.emptyString(" a ") == False
+
+def test_trimEmpty():
+    assert fp.trimEmpty("  ") == True
+    assert fp.trimEmpty("") == True
+    assert fp.trimEmpty(" a ") == False
+    assert fp.trimEmpty(None) == True
+
+def test_map():
+    assert fp.map(lambda x: x+1)([1,2,3]) == [2,3,4]
+
+def test_filter():
+    assert fp.filter(lambda x: x!=2)([1,2,3]) == [1,3,]
 
 def test_compose():
     addOne = lambda x: x+1
@@ -46,9 +88,30 @@ def test_chain():
     assert fp.chainIfElse(inc, dec)(good) == 2
     assert fp.chainIfElse(inc, dec)(bad) == 4
     assert fp.chainWhen(inc)(good) == 2
-    assert fp.chainUnless(inc)(good) == 1
-    assert fp.chainWhen(inc)(bad) == 5
+    assert fp.chainUnless(inc)(good) == good
+    assert fp.chainWhen(inc)(bad) == bad
     assert fp.chainUnless(inc)(bad) == 6
 
-def test_inc():
-    assert fp.inc(1) == 2
+def test_maybe():
+    isOne = lambda x: x == 1
+    addOne = lambda x: x + 1
+    assert fp.maybe(isOne, addOne, lambda x: x)(1) == {"ok": True, "data": 2}
+    assert fp.maybe(isOne, addOne, lambda x: x+2)(5) == {"ok": False, "data": 7}
+
+def test_validate():
+    x = {"name": "bob", "foo": "bar"}
+    checks = [
+        fp.check("name", lambda x: x=="bob", "it's bob"),
+        fp.check("age", lambda x: x is None, "age needed"),
+        fp.check("foo", lambda x: x != "bar", "foobar!"),
+    ]
+    assert fp.validate(checks)(x) == {
+        "ok": False, "data": ["it's bob", "age needed"]
+    }
+    assert fp.validate(checks)({**x, **{"age": 10}}) == {
+        "ok": False, "data": ["it's bob",]
+    }
+    good = {"name": "john", "age": 10, "foo": "bar"}
+    assert fp.validate(checks)(good) == {
+        "ok": True, "data": good
+    }
